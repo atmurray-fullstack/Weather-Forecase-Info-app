76cb10265f9b215dd42279d6b7e2708c3a071c44
Andrew Murray <atmurray@bellsouth.net>
2020-04-06 16:03:19 -0400

    added div creaton for previous searches and localStorage for last Search

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ var cityArr = [];
 var city = '';
 var state = '';
 var iconUrl = ''
+function getTime() {
+    return moment().format('L');
+}
 
+var currentDate = getTime();
 
 
 
@@ -14,27 +18,13 @@ var iconUrl = ''
 
 // var iconURL = 'http://openweathermap.org/img/wn/'+icon+'@2x.png'
 var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=819c140350303d362f2ba760003b1335'
-
-
-function getTime() {
-    return moment().format('L');
-}
-
-var currentDate = getTime();
 console.log(currentDate);
 
-
-
-
-
-
-
-$('#searchButton').on('click', function (event) {
-    event.preventDefault();
-    city = $('#inputCity').val().toUpperCase();
+if (localStorage.getItem('city') !== null) {
+    city = localStorage.getItem('city');
     console.log(city);
     queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=819c140350303d362f2ba760003b1335'
-
+    currentDate = getTime();
 
     /////////////////  ajax call to get current forecast information set to cityresultsObj////////////////////
     $.ajax({
@@ -43,26 +33,71 @@ $('#searchButton').on('click', function (event) {
     }).then(function (response) {
         console.log(response);
         cityresultsObj = new Object();
-        cityresultsObj.temp = response.main.temp;
+        cityresultsObj.temp = Math.floor((response.main.temp - 273.15) * (9 / 5) + 32);
         cityresultsObj.humidity = response.main.humidity;
         cityresultsObj.windSpeed = response.wind.speed;
         cityresultsObj.icon = response.weather[0].icon;
 
-        $('#cityDisplay').text(city);
-        $('#tempDisplay').text('Temperature: '+cityresultsObj.temp);
-        $('#humidityDisplay').text('Humidity: '+cityresultsObj.humidity);
-        $('#windSpeedDisplay').text('Wind Speed: '+cityresultsObj.windSpeed);
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
 
+        $('#cityDisplay').text(city + '   ' + currentDate);
+        $('#tempDisplay').text('Temperature: ' + cityresultsObj.temp + ' F');
+        $('#humidityDisplay').text('Humidity: ' + cityresultsObj.humidity + ' %');
+        $('#windSpeedDisplay').text('Wind Speed: ' + cityresultsObj.windSpeed + ' mph');
+
+
+        ////////////////////ajax call to get uvindex  //////////////////////////////////////
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/uvi/forecast?appid=' + apiKey + '&lat=' + lat + '&lon=' + lon + '&cnt=1',
+            method: "GET"
+        }).then(function (response) {
+            cityresultsObj.uvIndex = response[0].value;
+            $('#uvIndexDisplay').text('UV Index: ' + cityresultsObj.uvIndex)
+        })
+        var newDiv = $('<div>').text(city);
+        newDiv.addClass('prevSearch');
+        $('#searched').prepend(newDiv);
+        localStorage.setItem('city', city)
+        cityArr.push([city, cityresultsObj]);
+        console.log(cityArr)
+
+    })
+}
+
+
+
+
+
+
+///////search button on click event////////////////
+$('#searchButton').on('click', function (event) {
+    event.preventDefault();
+    city = $('#inputCity').val().toUpperCase();
+    console.log(city);
+    queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=819c140350303d362f2ba760003b1335'
+    currentDate = getTime();
+
+    /////////////////  ajax call to get current forecast information set to cityresultsObj////////////////////
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        cityresultsObj = new Object();
+        cityresultsObj.temp = Math.floor((response.main.temp - 273.15) * (9 / 5) + 32);
+        cityresultsObj.humidity = response.main.humidity;
+        cityresultsObj.windSpeed = response.wind.speed;
+        cityresultsObj.icon = response.weather[0].icon;
 
         var lat = response.coord.lat;
         var lon = response.coord.lon;
 
-        // console.log(cityresultsObj.temp)
-        // console.log(cityresultsObj.humidity)
-        // console.log(cityresultsObj.windSpeed)
-        // console.log(cityresultsObj.icon)
-        // console.log(lat);
-        // console.log(lon);
+        $('#cityDisplay').text(city + '   ' + currentDate);
+        $('#tempDisplay').text('Temperature: ' + cityresultsObj.temp + ' F');
+        $('#humidityDisplay').text('Humidity: ' + cityresultsObj.humidity + ' %');
+        $('#windSpeedDisplay').text('Wind Speed: ' + cityresultsObj.windSpeed + ' mph');
+
 
         ////////////////////ajax call to get uvindex  //////////////////////////////////////
         $.ajax({
@@ -71,23 +106,19 @@ $('#searchButton').on('click', function (event) {
         }).then(function (response) {
 
             cityresultsObj.uvIndex = response[0].value;
-            $('#uvIndexDisplay').text('UV Index: '+cityresultsObj.uvIndex)
-            // console.log(cityresultsObj.uvIndex);
+            $('#uvIndexDisplay').text('UV Index: ' + cityresultsObj.uvIndex)
         })
 
         cityArr.push([city, cityresultsObj]);
         console.log(cityArr);
 
-        // console.log(response.main)
-        // console.log(response.weather)
-    });
-    ///////////////assign h# elements///////////////
-    // $('#cityDisplay').text(city);
-    // $('#tempDisplay').text(cityresultsObj.temp);
-    // $('#humidityDisplay').text(cityresultsObj.humidity);
-    // $('#windSpeedDisplay').text(cityresultsObj.windSpeed);
-    // $('#uvIndexDisplay').text(cityresultsObj.uvIndex)
 
+    });
+    ///////  APPEND NEW ELEMENTS INSIDE CLICK EVENT//////////
+    var newDiv = $('<div>').text(city);
+    newDiv.addClass('prevSearch');
+    $('#searched').prepend(newDiv);
+    localStorage.setItem('city', city)
 
 })
 
